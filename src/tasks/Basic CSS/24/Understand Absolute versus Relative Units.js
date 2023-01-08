002a1e5edbd36305cb80dffda0b7e5a97649bd45
Sene4kaaa <senkevich92@mail.ru>
2023-01-08 15:33:24 +0300

    Create Understand Absolute versus Relative Units.js

---
+++ src/tasks/Basic CSS/24/Understand Absolute versus Relative Units.js
@@ -0,0 +1,35 @@
+// <style>
+//     .injected-text {
+//     margin - bottom: -25px;
+//     text-align: center;
+// }
+//
+//     .box {
+//     border - style: solid;
+//     border-color: black;
+//     border-width: 5px;
+//     text-align: center;
+// }
+//
+//     .yellow-box {
+//     background - color: yellow;
+//     padding: 20px 40px 20px 40px;
+// }
+//
+//     .red-box {
+//     background - color: red;
+//     margin: 20px 40px 20px 40px;
+//
+// }
+//
+//     .green-box {
+//     background - color: green;
+//     margin: 20px 40px 20px 40px;
+// }
+// </style>
+// <h5 className="injected-text">margin</h5>
+//
+// <div className="box yellow-box">
+//     <h5 className="box red-box">padding</h5>
+//     <h5 className="box green-box">padding</h5>
+// </div>
