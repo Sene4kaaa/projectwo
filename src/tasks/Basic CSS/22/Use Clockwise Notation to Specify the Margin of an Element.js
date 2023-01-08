ae88e8ee90deeae1e478376aafe4a554707bb26b
Sene4kaaa <senkevich92@mail.ru>
2023-01-08 15:29:40 +0300

    Create Use Clockwise Notation to Specify the Margin of an Element.js

---
+++ src/tasks/Basic CSS/22/Use Clockwise Notation to Specify the Margin of an Element.js
@@ -0,0 +1,35 @@
+// <style>
+//     .injected-text {
+//     margin - bottom: -25px;
+//     text-align: center;
+// }
+//
+//     .box {
+//     border - style: solid;
+//     border-color: black;
+//     border-width: 5px;
+//     text-align: center;
+// }
+//
+//     .yellow-box {
+//     background - color: yellow;
+//     padding: 20px 40px 20px 40px;
+// }
+//
+//     .red-box {
+//     background - color: crimson;
+//     color: #fff;
+//     margin: 20px 40px 20px 40px;
+// }
+//
+//     .blue-box {
+//     background - color: blue;
+//     color: #fff;
+// }
+// </style>
+// <h5 className="injected-text">margin</h5>
+//
+// <div className="box yellow-box">
+//     <h5 className="box red-box">padding</h5>
+//     <h5 className="box blue-box">padding</h5>
+// </div>
