9be951474fe054d208fd1e5e8790684aa80760d1
Sene4kaaa <senkevich92@mail.ru>
2023-01-08 15:23:12 +0300

    Create Adjust the Margin of an Element.js

---
+++ src/tasks/Basic CSS/17/Adjust the Margin of an Element.js
@@ -0,0 +1,38 @@
+// <style>
+//     .injected-text {
+//     margin - bottom: -25px;
+//     text-align: center;
+// }
+//
+//     .box {
+//     border - style: solid;
+//     border-color: black;
+//     border-width: 5px;
+//     text-align: center;
+// }
+//
+//     .yellow-box {
+//     background - color: yellow;
+//     padding: 10px;
+// }
+//
+//     .red-box {
+//     background - color: crimson;
+//     color: #fff;
+//     padding: 20px;
+//     margin: 20px;
+// }
+//
+//     .blue-box {
+//     background - color: blue;
+//     color: #fff;
+//     padding: 20px;
+//     margin: 10px;
+// }
+// </style>
+// <h5 className="injected-text">margin</h5>
+//
+// <div className="box yellow-box">
+//     <h5 className="box red-box">padding</h5>
+//     <h5 className="box blue-box">padding</h5>
+// </div>
